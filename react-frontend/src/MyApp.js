8d28c218ab6ea23a9bdbee8e1b5e9aedd39a35b5
Madelyn Cruz <mcruz42@calpoly.edu>
2023-01-17 13:49:51 -0800

    form exists! 5 done

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
-    return (
-      <div className="container">
-        <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      </div>
-    )  
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form />
+    </div>
+  )
 
 function removeOneCharacter (index) {
   const updated = characters.filter((character, i) => {
