0213ba55bf698b884537d6817e5f9481ac341a98
Madelyn Cruz <mcruz42@calpoly.edu>
2023-01-22 20:46:56 -0800

    before id work starts

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -29,7 +29,7 @@ function removeOneCharacter (index) {
 
   function updateList(person) { 
     makePostCall(person).then( result => {
-    if (result && result.status === 200)
+    if (result && result.status === 201)
        setCharacters([...characters, person] );
     });
  }
